ccb23f0f0ed41b9868d9d9fa7417f96b89519de2
sandy9707 <Sandy970701@Gmail.com>
2023-03-17 15:05:22 +0800

    2023-03-17 15-05-22

---
+++ shinyAPP.R
@@ -0,0 +1,62 @@
+library(shiny)
+library(xml2)
+
+# Define UI
+ui <- fluidPage(
+    titlePanel("GEO File Downloader"),
+    sidebarLayout(
+        sidebarPanel(
+            textInput("geoID", "Enter a GEO accession number:"),
+            uiOutput("fileType"),
+            actionButton("submitBtn", "Submit")
+        ),
+        mainPanel(
+            tableOutput("fileList")
+        )
+    )
+)
+
+# Define server
+server <- function(input, output) {
+    # Define function to get directory listing
+    getDirListing <- function(url) {
+        a <- read_html(url)
+        fnames <- grep("^G", xml_text(xml_find_all(a, "//a/@href")), value = TRUE)
+        return(fnames)
+    }
+
+    # Determine file type options based on input type
+    output$fileType <- renderUI({
+        if (grepl("^GSE", input$geoID)) {
+            selectInput("fileOption", "Choose file type:", c("matrix", "suppl"))
+        } else if (grepl("^GPL", input$geoID)) {
+            selectInput("fileOption", "Choose file type:", "annot")
+        }
+    })
+
+    # Get list of files and display in table when submit button is clicked
+    output$fileList <- renderTable({
+        req(input$submitBtn)
+        if (input$fileOption == "matrix") {
+            gdsurl <- "https://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/matrix/"
+        } else if (input$fileOption == "suppl") {
+            gdsurl <- "https://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/suppl/"
+        } else if (input$fileOption == "annot") {
+            gdsurl <- "https://ftp.ncbi.nlm.nih.gov/geo/platforms/%s/%s/annot/"
+        }
+        stub <- gsub("\\d{1,3}$", "nnn", input$geoID, perl = TRUE)
+        b <- getDirListing(sprintf(gdsurl, stub, input$geoID))
+        ret <- list()
+        for (x in 1:length(b)) {
+            ret[[x]] <- sprintf(
+                paste0(gdsurl, "%s"),
+                stub, input$geoID, b[x]
+            )
+        }
+        merge <- c(unlist(ret))
+        data.frame("File Name" = merge)
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
